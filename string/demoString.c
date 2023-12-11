c35624940c90466a60e7845ab9e6db3713beda17
wenliang <wenliang@163.com>
2023-12-11 11:13:14 +0800

    [新增] 新增字符数组和字符串的区别&内存分区

---
+++ string/demoString.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE   32
+
+/* 作业1: 手写字符串strlen strcpy, strcmp, strcat */
+
+
+int main()
+{
+#if 0
+    /* 数组的初始化 */
+    /* 字符数组 */
+    char array[BUFFER_SIZE];
+    /* 数组初始化 */
+    memset(array, 0, sizeof(array));
+    
+    int len = sizeof(array);
+    printf("len:%d\n", len);    
+#endif
+
+#if 0
+    /* 字符数组 */
+    char array[BUFFER_SIZE] = "hello world";    
+    int len = sizeof(array);
+    printf("len:%d, array:%s\n", len, array);
+#endif
+
+#if 0
+    /* 字符串 (字符数组) */
+    char array1[BUFFER_SIZE] = "hello world";    
+    /* 第一个 strlen */
+    int len = strlen(array1);
+    printf("len:%d\n", len);
+
+    /* 第二个 strcpy */
+    char name[BUFFER_SIZE];
+    memset(name, 0, sizeof(name));
+    strcpy(name, "zhangSan");
+    printf("name:%s\n", name);
+
+    /* 第三个 strcat */
+    strcat(name, " is a boy");
+    printf("name:%s\n", name);
+
+    /* 第四个 strcmp */
+    char name2[BUFFER_SIZE] = "zhangsan";
+    int ret = strcmp(name, name2);
+    printf("ret:%d\n", ret);
+#endif      
+    
+    
+#if 0
+    /* 字符串(常量) */
+    /* 指针出入 */
+    char * ptr = "hello world";
+    int len = strlen(ptr);
+    int size = sizeof(ptr);
+
+    printf("len:%d\n", len);
+    printf("size:%d\n", size);
+
+    printf("*ptr:%c\t*(ptr+1):%c\tptr:%s\n", *ptr, *(ptr+1), ptr); 
+
+    *(ptr + 1) = 'H';
+    printf("*ptr:%c\t*(ptr+1):%c\tptr:%s\n", *ptr, *(ptr+1), ptr); 
+#endif
+
+
+    return 0;
+}
